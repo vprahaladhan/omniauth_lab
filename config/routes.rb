948c7bf99575ab4df45eb10fbaa923dd87cd3777
prahal <vprahaladhan@gmail.com>
2021-06-16 14:55:03 +1000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get '/', to: 'welcome#home'
+  get 'welcome/home', as: 'root'
   # Add your routes here
+  match '/auth/github/callback', to: 'sessions#create', via: [:get, :post]
 end
